946176c4bf1abc47ee92768ff56a19d16c84f607
Daniel Hwang <dawh@ines.home>
2026-02-06 04:15:26 -0600

    Add zephyr and zephyr-nix

--- flake.nix
+++ flake.nix
@@ -4,6 +4,15 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
+
+    # This pins requirements.txt provided by zephyr-nix.pythonEnv.
+    zephyr.url = "github:zmkfirmware/zephyr/v3.5.0+zmk-fixes";
+    zephyr.flake = false;
+
+    # Zephyr sdk and toolchain.
+    zephyr-nix.url = "github:nix-community/zephyr-nix";
+    zephyr-nix.inputs.nixpkgs.follows = "nixpkgs";
+    zephyr-nix.inputs.zephyr.follows = "zephyr";
   };
 
   outputs = inputs @ {flake-parts, ...}:
@@ -15,13 +24,20 @@
 
       systems = ["x86_64-linux"];
       perSystem = {
+        inputs',
         config,
         pkgs,
         ...
       }: {
         devshells.default = {
           packages = with pkgs; [
-            just
+            (inputs'.zephyr-nix.packages.sdk.override {
+              targets = [
+                "arm-zephyr-eabi"
+              ];
+            })
+            inputs'.zephyr-nix.packages.pythonEnv
+            inputs'.zephyr-nix.packages.hosttools-nix
           ];
         };
         formatter = pkgs.alejandra;
